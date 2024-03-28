05a59cb2adef76db9bea9bba10fc2a03968d17bb
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-28 14:05:08 -0700

    Merge code with main
    This was done to enable testing to the saved recipes page. Note: the saved recipes page currently doesn't work

--- src/scripts.js
+++ src/scripts.js
@@ -6,9 +6,21 @@ import "./styles.css";
 import ingredientsData from "./data/ingredients";
 import "./images/turing-logo.png";
 // Below are examples of how you can import functions from either the recipes or domUpdates files.
+import recipeData from "./data/recipes";
+import usersData from "./data/users";
 import { displayRecipes } from "./domUpdates";
 import { findRecipeIngredients } from "./recipes";
 
 // all the favorite recipes should be stored as recipe objects here
 export const favoriteRecipes = [];
+global.favoriteRecipes = favoriteRecipes;
+export const currentUser = getRandomUser(usersData);
+global.currentUser = currentUser;
 
+function getRandomUser(user_dataset) {
+  return user_dataset[randomNumber(user_dataset.length)];
+}
+
+function randomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
