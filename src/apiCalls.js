25c300d1313fbd64c333e0d5c816466bde718c09
laurel Bonal <laurelbonal@gmail.com>
2024-03-28 15:37:51 -0600

    Add API calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,35 @@
 // Your fetch requests will live here!
 
 
+function getUser(){
+    let users;
+    fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users')
+    .then(r => r.json())
+    .then(data => users = data)
+    .catch(error => console.log(error))
+    return users
+}
+
+function getIngredients(){
+    let ingredients;
+    fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients')
+    .then(r => r.json())
+    .then(data => ingredients = data)
+    .catch(error => console.log(error))
+    return ingredients
+}
+
+function getRecipes(){
+    let recipes;
+    fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes')
+    .then(r => r.json())
+    .then(data => recipes = data)
+    .catch(error => console.log(error))
+    return recipes
+}
+
+
+
+
 console.log('I will be a fetch request!')
+
