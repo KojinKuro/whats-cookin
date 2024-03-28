b20a18bfb67a338b0ad566c551a381b9e438dfa2
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-28 15:39:09 -0700

    Feature: enable api calls to load data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,48 +1,51 @@
 // Your fetch requests will live here!
 
-export let usersAPIData = []
-export let recipesAPIData = []
-export let ingredientsAPIData = []
+export let usersAPIData = [];
+export let recipesAPIData = [];
+export let ingredientsAPIData = [];
 
-global.usersAPIData = usersAPIData
-global.recipesAPIData = recipesAPIData
-global.ingredientsAPIData = ingredientsAPIData
+// global.usersAPIData = usersAPIData;
+// global.recipesAPIData = recipesAPIData;
+// global.ingredientsAPIData = ingredientsAPIData;
 
-
-function loadUser(){
-    return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users')
-    .then(r => r.json())
-    .catch(error => console.log(error))
+function loadUser() {
+  return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
+    .then((r) => r.json())
+    .then((data) => data.users)
+    .catch((error) => console.log(error));
 }
 
-function loadIngredients(){
-    return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients')
-    .then(r => r.json())
-    .catch(error => console.log(error))
+function loadIngredients() {
+  return fetch(
+    "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients"
+  )
+    .then((r) => r.json())
+    .then((data) => data.ingredients)
+    .catch((error) => console.log(error));
 }
 
-function loadRecipes(){
-    return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes')
-    .then(r => r.json())
-    .catch(error => console.log(error))
+function loadRecipes() {
+  return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
+    .then((r) => r.json())
+    .then((data) => data.recipes)
+    .catch((error) => console.log(error));
 }
 
-
-
-function loadData(){
-    Promise.all([loadUser(), loadIngredients(), loadRecipes()]).then((values) => {
-      console.log(values)
-      console.log(values[0])
-        usersAPIData = values[0]
-       
-        ingredientsAPIData = values[1].ingredients
-        recipesAPIData = values[2].recipes   
-    })
+function loadData() {
+  Promise.all([loadUser(), loadIngredients(), loadRecipes()]).then(
+    ([user, ingredient, recipes]) => {
+      usersAPIData = user;
+      ingredientsAPIData = ingredient;
+      recipesAPIData = recipes;
+    }
+  );
 }
 
-global.loadData = loadData
+function printData() {
+  console.log(usersAPIData);
+  console.log(ingredientsAPIData);
+  console.log(recipesAPIData);
+}
 
-
-
-console.log('I will be a fetch request!')
-
+global.loadData = loadData;
+global.printData = printData;
