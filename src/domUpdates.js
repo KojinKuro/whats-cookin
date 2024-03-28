d54e45096d3c8647103540da18bb537ebf7269a7
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-28 12:00:08 -0700

    Format file

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,13 +2,13 @@ import { filterRecipeByTag, getTagRecipeCount } from "../src/tags";
 import ingredientsData from "./data/ingredients";
 import recipeData from "./data/recipes";
 import {
+  addRecipeToArray,
   findRecipeFromID,
   findRecipeIngredients,
   findRecipeIngredientsQuantity,
   findRecipeInstructions,
-  addRecipeToArray,
+  isRecipeFavorited,
   removeRecipeFromArray,
-  isRecipeFavorited
 } from "./recipes";
 import { favoriteRecipes } from "./scripts";
 import { search } from "./search";
@@ -21,9 +21,10 @@ const main = document.querySelector("main");
 const mainDirectory = document.getElementById("directory-page");
 const mainRecipe = document.getElementById("recipe-page");
 const filterSection = document.querySelector("nav.filter-container");
-const searchBox = document.querySelector(".search-box")
+const searchBox = document.querySelector(".search-box");
 
-const heartOn = '<svg class="heart" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #b30202;transform: ;msFilter:;"><path d="M20.205 4.791a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412L12 21.414l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416z"></path></svg>'
+const heartOn =
+  '<svg class="heart" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #b30202;transform: ;msFilter:;"><path d="M20.205 4.791a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412L12 21.414l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416z"></path></svg>';
 const heartOff = `<svg class="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
 style="
   fill: rgba(157, 150, 139, 1);
@@ -31,8 +32,7 @@ style="
   msFilter: 'progid:DXImageTransform.Microsoft.BasicImage(rotation=0, mirror=1)';
 ">
 <path d="M12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412l7.332 7.332c.17.299.498.492.875.492a.99.99 0 0 0 .792-.409l7.415-7.415c2.354-2.354 2.354-6.049-.002-8.416a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595zm6.791 1.61c1.563 1.571 1.564 4.025.002 5.588L12 18.586l-6.793-6.793c-1.562-1.563-1.561-4.017-.002-5.584.76-.756 1.754-1.172 2.799-1.172s2.035.416 2.789 1.17l.5.5a.999.999 0 0 0 1.414 0l.5-.5c1.512-1.509 4.074-1.505 5.584-.002z"></path>
-</svg>`
-
+</svg>`;
 
 // EVENT LISTENERS
 addEventListener("load", init);
@@ -50,24 +50,23 @@ mainDirectory.addEventListener("click", (e) => {
   if (!e.target.closest(".recipe-card")) return;
   const clickedRecipe = e.target.closest(".recipe-card");
   const recipe = findRecipeFromID(clickedRecipe.dataset.id, recipeData);
-  if (e.target.classList.contains('heart')){
-    const heartContainer = e.target.closest('.heart-container')
-    heartContainer.innerHTML = ''
-    if (!isRecipeFavorited(recipe, favoriteRecipes)){
-      heartContainer.innerHTML = heartOn
-      addRecipeToArray(favoriteRecipes, recipe)
+  if (e.target.classList.contains("heart")) {
+    const heartContainer = e.target.closest(".heart-container");
+    heartContainer.innerHTML = "";
+    if (!isRecipeFavorited(recipe, favoriteRecipes)) {
+      heartContainer.innerHTML = heartOn;
+      addRecipeToArray(favoriteRecipes, recipe);
     } else {
-      heartContainer.innerHTML = heartOff
-      removeRecipeFromArray(favoriteRecipes, recipe)
+      heartContainer.innerHTML = heartOff;
+      removeRecipeFromArray(favoriteRecipes, recipe);
     }
-    
-  } else { main.innerHTML = "";
-  main.append(createRecipePageHTML(recipe));
-  main.setAttribute("id", "recipe-page");
-  filterSection.classList.add("hidden");
+  } else {
+    main.innerHTML = "";
+    main.append(createRecipePageHTML(recipe));
+    main.setAttribute("id", "recipe-page");
+    filterSection.classList.add("hidden");
   }
 });
-
 
 // FUNCTIONS
 function init() {
@@ -132,16 +131,16 @@ function createRecipeHTML(recipe) {
       <span class="label"> ingredients </span>
       ${findRecipeIngredients(recipe, ingredientsData).join(", ")}
     </h3>
-    </div>`
+    </div>`;
 
-   const currentHeart = article.querySelector('.heart-container')
-   currentHeart.innerHTML = ''
-   if (isRecipeFavorited(recipe, favoriteRecipes)){
-    currentHeart.innerHTML = heartOn
-    addRecipeToArray(favoriteRecipes, recipe)
+  const currentHeart = article.querySelector(".heart-container");
+  currentHeart.innerHTML = "";
+  if (isRecipeFavorited(recipe, favoriteRecipes)) {
+    currentHeart.innerHTML = heartOn;
+    addRecipeToArray(favoriteRecipes, recipe);
   } else {
-    currentHeart.innerHTML = heartOff
-    removeRecipeFromArray(favoriteRecipes, recipe)
+    currentHeart.innerHTML = heartOff;
+    removeRecipeFromArray(favoriteRecipes, recipe);
   }
 
   return article;
@@ -233,7 +232,5 @@ function filterRecipes() {
   displayRecipes(recipesToDisplay);
   updateTagsToDOM();
 }
-
-
 
 export { displayRecipes };
