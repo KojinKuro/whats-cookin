024f982b65b38af03f9012f68fe2118c1c067410
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-28 17:18:19 -0700

    Remove old imports

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,10 @@
 //NOTE: Data model and non-dom manipulating logic will live in this file.
 
-import apiCalls from "./apiCalls";
+import "./apiCalls";
 import "./styles.css";
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import ingredientsData from "./data/ingredients";
-import "./images/turing-logo.png";
-// Below are examples of how you can import functions from either the recipes or domUpdates files.
-import recipeData from "./data/recipes";
-import usersData from "./data/users";
-import { displayRecipes } from "./domUpdates";
-import { findRecipeIngredients } from "./recipes";
+// // An example of how you tell webpack to use an image (also need to link to it in the index.html)
+// import "./images/turing-logo.png";
+// // Below are examples of how you can import functions from either the recipes or domUpdates files.
 
 // all the favorite recipes should be stored as recipe objects here
 export const favoriteRecipes = [];
