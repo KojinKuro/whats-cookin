790c3d2c57f6b56f50958137ff3e0fa2544b2542
laurel Bonal <laurelbonal@gmail.com>
2024-03-29 12:47:13 -0600

    Add functionality to random recipe button

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -27,6 +27,7 @@ const filterSection = document.querySelector("nav.filter-container");
 const searchBox = document.querySelector(".search-box");
 const cookbookButton = document.querySelector(".cookbook");
 const savedRecipesButton = document.querySelector(".saved-recipes");
+const randomRecipeButton = document.querySelector(".random-recipe")
 
 const heartOn =
   '<svg class="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="fill: #b30202;transform: ;msFilter:;"><path d="M20.205 4.791a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412L12 21.414l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416z"></path></svg>';
@@ -68,6 +69,7 @@ mainDirectory.addEventListener("click", (e) => {
     filterSection.classList.add("hidden");
   }
 });
+randomRecipeButton.addEventListener('click', displayRandomRecipe)
 
 cookbookButton.addEventListener("click", function () {
   isSavedRecipesView = false;
@@ -321,6 +323,23 @@ function displaySavedRecipes(recipes) {
     main.innerHTML =
       '<div style="text-align: center; font-family: Gatile, sans-serif; font-size: 5vh; color: #333;">No saved recipes found.</div>';
   }
+}
+
+function displayRandomRecipe() {
+  const randomIndex = Math.floor(Math.random() * recipeData.length);
+  const randomRecipe = recipeData[randomIndex];
+
+  main.innerHTML = "";
+  main.append(createRecipePageHTML(randomRecipe));
+  main.setAttribute("id", "recipe-page");
+  filterSection.classList.add("hidden");
+
+  const heartContainer = document.querySelector(".heart-container");
+
+  heartContainer.addEventListener("click", () => {
+    const heartIcon = heartContainer.querySelector(".heart");
+    toggleHeart(heartIcon, randomRecipe, favoriteRecipes);
+  });
 }
 
 export { displayRecipes };
