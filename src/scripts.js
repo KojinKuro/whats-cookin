4341bd781696e7cfc45771f7e6f8dd0f41e642af
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-04-10 07:41:24 -0700

    Update webpack and code to use Sass

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,7 @@
 //NOTE: Data model and non-dom manipulating logic will live in this file.
 
 import "./apiCalls";
-import "./styles.css";
+import "./styles.scss";
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import "./images/turing-logo.png";
 // Below are examples of how you can import functions from either the recipes or domUpdates files.
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,40 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-  "mode": "none",
-  "entry": "./src/scripts.js",
-  "output": {
-    "path": __dirname + '/dist',
-    "filename": "bundle.js",
-    sourceMapFilename: "bundle.js.map"
+  mode: "none",
+  entry: "./src/scripts.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+    sourceMapFilename: "bundle.js.map",
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist')
+    contentBase: path.join(__dirname, "dist"),
   },
-  "devtool": "source-map",
+  devtool: "source-map",
   // CSS and file (image) loaders
-  "module": {
-    "rules": [
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'images/',
-              publicPath: 'images/'
-            }
-          }
-        ]
-      }
-    ]
-  }
+              name: "[name].[ext]",
+              outputPath: "images/",
+              publicPath: "images/",
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
