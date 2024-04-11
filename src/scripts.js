58a689b249b9f7f7f8472802505beb153d0b9d89
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-04-11 11:51:28 -0700

    refactor: Make fetching DRY

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,6 @@
 //NOTE: Data model and non-dom manipulating logic will live in this file.
 
-import "./apiCalls";
+import "./domUpdates";
 import "./styles.scss";
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import "./images/turing-logo.png";
