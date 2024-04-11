b454e3916cf981caa47f2717ea0c38cd2e551f76
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-04-11 11:22:46 -0700

    refactor: Add random recipes to improve test

--- test/favorite-recipe-test.js
+++ test/favorite-recipe-test.js
@@ -1,25 +1,29 @@
 import { expect } from "chai";
 import { isFavorited } from "../src/recipes";
-import { recipe1, recipe2 } from "./data/mockData";
+import {
+  mockRecipeDataset,
+  recipe1,
+  recipe2,
+  recipe3,
+  recipe4,
+} from "./data/mockData";
 
 describe("isFavorited", () => {
-  const recipe_dataset = [recipe1, recipe2];
-
   it("returns favorited recipes that exist in the dataset #1", () => {
-    const favoriteRecipes = [recipe1, recipe2];
-    const isFavored = isFavorited(favoriteRecipes, recipe_dataset);
+    const favoriteRecipes = [recipe1, recipe2, { id: 32345 }, { id: 23194985 }];
+    const isFavored = isFavorited(favoriteRecipes, mockRecipeDataset);
     expect(isFavored).to.deep.equal([recipe1, recipe2]);
   });
 
   it("returns favorited recipes that exist in the dataset #2", () => {
-    const favoriteRecipes = [recipe1];
-    const isFavored = isFavorited(favoriteRecipes, recipe_dataset);
-    expect(isFavored).to.deep.equal([recipe1]);
+    const favoriteRecipes = [recipe3, { id: 1245 }];
+    const isFavored = isFavorited(favoriteRecipes, mockRecipeDataset);
+    expect(isFavored).to.deep.equal([recipe3]);
   });
 
   it("returns an empty array if no favorited recipes are found in the dataset", () => {
-    const favoriteRecipes = [];
-    const isFavored = isFavorited(favoriteRecipes, recipe_dataset);
+    const favoriteRecipes = [{ id: 218412 }, { id: 12381495 }];
+    const isFavored = isFavorited(favoriteRecipes, mockRecipeDataset);
     expect(isFavored).to.deep.equal([]);
   });
 });
